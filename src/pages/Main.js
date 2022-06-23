387d08bcc66ae4860d0d41cd15f2f65386141808
whddnjs1715 <whddnjs1715@gmail.com>
2022-06-23 10:37:17 +0900

    initial commit

---
+++ src/pages/Main.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+
+const Main = () => {
+    return     <div className="container">
+    <div className="inner">
+    </div>
+  </div>;
+  };
+  
+  export default Main;
